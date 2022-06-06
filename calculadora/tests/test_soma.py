1761bb545eedd31746445406115614801b34a85e
BrayanMota <brayanmota2001@hotmail.com>
2022-06-06 11:54:08 -0300

    First commit

--- calculadora/tests/test_soma.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
-
-def test_soma():
-    response = client.get("/api/adicao?numero1=10&numero2=20")
-    assert response.status_code == 200
-    assert response == {"Resultado da Soma": "30"}
